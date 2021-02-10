7a3d0c8c3335168c97a109d2683d0fac792d7b81
B. Ricks <bricks@unomaha.edu>
2021-02-10 13:14:28 -0600

    Add Day 08 Starter

---
+++ Code/Starter Day08/game/first-scene.js
@@ -0,0 +1,15 @@
+import Scene from "../engine/scene.js"
+
+/**
+ * @class FirstScene the first scene in our game
+ */
+class FirstScene extends Scene{
+    /**
+     * Call the super constructor and then initialize any local data
+     */
+    constructor() {
+        super();
+    }
+}
+
+export default FirstScene;
